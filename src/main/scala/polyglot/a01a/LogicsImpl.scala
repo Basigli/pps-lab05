6585c7b404f03068f1f04ac4b516e39a37d7f37a
Gabribrele <gabrielebasigli@gmail.com>
2025-05-29 09:36:39 +0200

    (feat) a01a: full logic

--- src/main/scala/polyglot/a01a/LogicsImpl.scala
+++ src/main/scala/polyglot/a01a/LogicsImpl.scala
@@ -8,17 +8,37 @@ import util.Sequences.*
 class LogicsImpl(private val size: Int, private val boat: Int) extends Logics:
   private var boatCoordinates: Sequence[(Int, Int)] = Sequence.empty
   randomizeBoat()
-
-
+  private var attempts = 5
+  private var hitCounter = 0
 
   private def randomizeBoat(): Unit =
     val rand = scala.util.Random
-    val initialCell = (rand.nextInt(size -1), rand.nextInt(size -1))  // for instance (2, 2)   boat = 4
-    // choose between a random direction (left or right) ?
+    val initialRow = rand.nextInt(size -1)  // for instance (2, 2)   boat = 4
+    var offset = rand.nextInt(size - 1 - boat)
+
+    for column <- 0 until boat do
+      boatCoordinates = Sequence.Cons((initialRow, column), boatCoordinates)
 
 
 
 
 
-  def hit(row: Int, col: Int) =
-    Result.HIT
+  def hit(row: Int, col: Int) = {
+    if (boatCoordinates.contains((row, col))) {
+      hitCounter = hitCounter + 1
+      if (hitCounter == boat) {
+        Result.WON
+      } else {
+        Result.HIT
+      }
+    } else {
+      attempts = attempts - 1
+      println("attempts " + attempts)
+      if (attempts == 0) {
+        Result.LOST
+      } else {
+        Result.MISS
+      }
+    }
+  }
+
